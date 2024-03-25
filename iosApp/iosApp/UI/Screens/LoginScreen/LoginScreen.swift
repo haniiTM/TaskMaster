cbab122077ae50c3a689ffb8f4627e7e330a8eb8
evilgen <aleynikov.ilya@list.ru>
2024-03-25 16:55:01 +0300

    updated project structure

---
+++ iosApp/iosApp/UI/Screens/LoginScreen/LoginScreen.swift
@@ -0,0 +1,19 @@
+//
+//  LoginScreen.swift
+//  iosApp
+//
+//  Created by evilgen on 25.03.2024.
+//  Copyright © 2024 TaskMaster. All rights reserved.
+//
+
+import SwiftUI
+
+struct LoginScreen: View {
+    var body: some View {
+        Text("hello there")
+    }
+}
+
+#Preview {
+    LoginScreen()
+}
