48f6152f60dff3874a3fa9e9bd005a7aef5fcc72
kurcevroman11 <kurcevroman11@gmail.com>
2024-04-25 11:42:01 +0300

    Добавлен флаг для админа или проект-менеджера

--- androidApp/src/main/java/com/example/taskmaster/android/ui/screens/login_screen/LoginViewModel.kt
+++ androidApp/src/main/java/com/example/taskmaster/android/ui/screens/login_screen/LoginViewModel.kt
@@ -13,30 +13,6 @@ import com.example.taskmaster.domain.repositories.AuthRepository
 import kotlinx.coroutines.launch
 
 class LoginViewModel constructor(private val authRepository: AuthRepository) : ViewModel() {
-//    private val _state = mutableStateOf(LoginViewModel.ItemAccessTokenDto())
-//    val state: State<LoginViewModel.ItemAccessTokenDto> = _state
-//
-//    fun dataToken(login: String, password: String) {
-//        viewModelScope.launch {
-//            try {
-//                _state.value = state.value.copy(isLoading = true)
-//                val tokenAndFlag = authRepository.fetchUserToken(login, password)
-//                _state.value = state.value.copy(
-//                    itemState = tokenAndFlag,
-//                    isLoading = false
-//                )
-//            } catch(e: Exception) {
-//                _state.value = state.value.copy(isLoading = false)
-//            }
-//        }
-//    }
-//
-//    data class ItemAccessTokenDto (
-//        val itemState: AccessTokenDto? = null,
-//        val isLoading: Boolean = false
-//    )
-
-
     suspend fun fetchUserToken(login: String, password: String): AccessTokenDto {
         return authRepository.fetchUserToken(login, password)
     }
