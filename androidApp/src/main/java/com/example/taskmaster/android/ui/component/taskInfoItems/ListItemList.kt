958e0cd79762c08491de1e02edf92f7c283ce2dd
Teo <iackov48@gmail.com>
2024-05-07 06:22:38 +0300

    Изменение верстки списка трудозатрат

--- androidApp/src/main/java/com/example/taskmaster/android/ui/component/taskInfoItems/ListItemList.kt
+++ androidApp/src/main/java/com/example/taskmaster/android/ui/component/taskInfoItems/ListItemList.kt
@@ -4,9 +4,10 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
@@ -17,18 +18,24 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun ListItemList() {
+    var laborCosts = listOf("1", "2", "3", "4", "5", "6", "7")
     Box(
         modifier = Modifier
             .padding(start = 14.dp, top = 26.dp, end = 14.dp)
             .fillMaxWidth()
             .clip(shape = RoundedCornerShape(15.dp))
-            .border(BorderStroke(1.dp, MaterialTheme.colorScheme.outline), shape = RoundedCornerShape(15.dp))
+            .border(
+                BorderStroke(1.dp, MaterialTheme.colorScheme.outline),
+                shape = RoundedCornerShape(15.dp)
+            )
             .background(Color.White)
     ) {
-        Column {
-            ListItem()
-            ListItem()
-            ListItem()
+        LazyColumn {
+            itemsIndexed(laborCosts) { _, item ->
+                if (item != null) {
+                    ListItem(number = item.toInt())
+                }
+            }
         }
     }
 }
