6711c970a798c856440d48c7c5da508656b5360d
evilGen <alejnikov.ilya@gmail.com>
2024-05-16 18:54:50 +0300

    change some gradle settings to improve performance

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,11 @@ plugins {
     //trick: for the same plugin versions in all sub-modules
     alias(libs.plugins.androidApplication).apply(false)
     alias(libs.plugins.androidLibrary).apply(false)
+
     alias(libs.plugins.kotlinAndroid).apply(false)
     alias(libs.plugins.kotlinMultiplatform).apply(false)
     alias(libs.plugins.kotlinCocoapods).apply(false)
-    kotlin("plugin.serialization") version "1.8.10" apply false
-    id("app.cash.sqldelight") version "2.0.0-alpha05" apply false
+
+    alias(libs.plugins.kotlinSerialization) apply false
+    alias(libs.plugins.sqlDelight) apply false
 }
