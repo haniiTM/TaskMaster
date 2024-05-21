d0f1183fd81a80e9abc1b59754f9f44488283ae8
ivan.yatskov <iackov48@gmail.com>
2024-05-21 17:58:22 +0300

    Изменение навигации при валидном токене и добавление функционал скачивания вложений

--- androidApp/src/main/java/com/example/taskmaster/android/ui/component/taskInfoItems/ListItem.kt
+++ androidApp/src/main/java/com/example/taskmaster/android/ui/component/taskInfoItems/ListItem.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.material3.Divider
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
@@ -24,8 +25,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import com.example.taskmaster.android.AndroidDownloader
@@ -49,6 +52,15 @@ fun ListItem(
     var showLaborCostInfo by remember {
         mutableStateOf(false)
     }
+    val screenWidth = LocalConfiguration.current.screenWidthDp.dp
+    val maxWidth = (screenWidth * 0.6f)
+    val parts = name.split(".")
+    var fileName = ""
+    var extension = ""
+    if (parts.size == 2) {
+        fileName = parts[0]
+        extension = "." + parts[1]
+    }
     val context = LocalContext.current
     var expanded by remember {
         mutableStateOf(false)
@@ -67,7 +79,12 @@ fun ListItem(
             verticalAlignment = Alignment.CenterVertically,
             modifier = Modifier.fillMaxWidth()
         ) {
-            Text(text = name, modifier = Modifier.padding(horizontal = 12.dp), color = Color.Black)
+            Row(horizontalArrangement = Arrangement.Start, modifier = Modifier.fillMaxWidth(.8f)) {
+                Text(text = fileName, modifier = Modifier
+                    .padding(start = 12.dp)
+                    .sizeIn(maxWidth = maxWidth), color = Color.Black, overflow = TextOverflow.Ellipsis)
+                Text(text = extension, color = Color.Black)
+            }
             if (attachmentsListFlag) {
                 DropdownMenuArea(
                     expanded = expanded,
